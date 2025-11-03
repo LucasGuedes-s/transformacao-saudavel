179b0954bcf700f852f96581b76f12a436dbe88b
Lucas Guedes <lucasguedes2908@gmail.com>
2025-11-02 22:11:53 -0300

    Alteração

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -89,7 +89,7 @@ const routes = [
   },
   {
     path: '/ebooks',
-    name: 'perfil',
+    name: 'ebooks',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
