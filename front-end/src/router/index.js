f8bd275443af0d9b7b70a728876f51ce341cce00
Lucas Guedes <lucasguedes2908@gmail.com>
2025-11-09 17:15:42 -0300

    Alteração de pagp

--- front-end/src/router/index.js
+++ front-end/src/router/index.js
@@ -59,7 +59,7 @@ const routes = [
   },
     {
     path: '/cadastro/pago',
-    name: 'cadastro',
+    name: 'cadastro-pago',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
